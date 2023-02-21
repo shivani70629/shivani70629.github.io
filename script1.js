24d7f97d8a5fb8699bf7f77b97b01d8e259ca3a7
shivani70629 <121672555+shivani70629@users.noreply.github.com>
2023-02-21 07:29:00 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,18 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "20%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+  function openNav1() {
+    document.getElementById("myNav2").style.width = "80%";
+  }
+  
+  function closeNav1() {
+    document.getElementById("myNav2").style.width = "0%";
+  }
+
+  
